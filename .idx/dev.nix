da895ce243ddd2cd7ac07848279f48b3d1fbbd62
hello <hello@flagseeker.com>
2024-10-04 18:16:59 +0000

    add dev.nix for google idx usage

---
+++ .idx/dev.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  env = { };
+  idx = {
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+      "tenninebt.vscode-koverage"
+      "ms-vscode.live-server"
+      "ryanluker.vscode-coverage-gutters"
+      "github.vscode-github-actions"
+      "esbenp.prettier-vscode"
+      "jock.svg"
+      "GitHub.vscode-pull-request-github"
+    ];
+    workspace = {
+      onCreate = { };
+    };
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT" ];
+          manager = "flutter";
+        };
+
+      };
+    };
+  };
+}
