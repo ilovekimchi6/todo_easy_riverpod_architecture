8ebd4cff8d053d71eb842db5f0f00f1a4eb3dfdf
hello <hello@flagseeker.com>
2024-10-05 19:16:37 +0000

    update sembase to in-memory

--- lib/core/common_providers/sembast_db.g.dart
+++ lib/core/common_providers/sembast_db.g.dart
@@ -8,7 +8,9 @@ part of 'sembast_db.dart';
 
 String _$sembastDatabaseHash() => r'06dbd28fa54b0b536bf3a9095bccdba1fc14690f';
 
-/// See also [sembastDatabase].
+/// Sembast Database is created using a `future provider`.
+///
+/// Copied from [sembastDatabase].
 @ProviderFor(sembastDatabase)
 final sembastDatabaseProvider = FutureProvider<Database>.internal(
   sembastDatabase,
