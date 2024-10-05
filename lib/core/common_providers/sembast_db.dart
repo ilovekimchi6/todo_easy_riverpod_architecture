9e89a5c5558f1b62ccceeb76c7a764d94f128c63
hello <hello@flagseeker.com>
2024-10-05 19:14:05 +0000

    update sembast db to in-memory db

--- lib/core/common_providers/sembast_db.dart
+++ lib/core/common_providers/sembast_db.dart
@@ -1,20 +1,10 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:sembast/sembast.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sembast/sembast_io.dart';
+import 'package:sembast/sembast_memory.dart';
 
 part 'sembast_db.g.dart';
 
 /// Sembast Database is created using a `future provider`.
 @Riverpod(keepAlive: true)
 Future<Database> sembastDatabase(SembastDatabaseRef ref) async {
-  var dir = await getApplicationDocumentsDirectory();
-  
-  /// Ensure that the directory exists.
-  await dir.create(recursive: true);
-  
-  var dbPath = join(dir.path, 'todo.db');
-  var db = await databaseFactoryIo.openDatabase(dbPath);
-  return db;
+  return databaseFactoryMemoryFs.openDatabase('todo.db');
 }
--- lib/core/common_providers/sembast_db.g.dart
+++ lib/core/common_providers/sembast_db.g.dart
@@ -6,7 +6,7 @@ part of 'sembast_db.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$sembastDatabaseHash() => r'cf8c0dd951c1dd81ae7903dc2894ed8b7902b45c';
+String _$sembastDatabaseHash() => r'06dbd28fa54b0b536bf3a9095bccdba1fc14690f';
 
 /// See also [sembastDatabase].
 @ProviderFor(sembastDatabase)
