2a8d7c81d3b6898558f717e37d6a35e0bd395e5e
hello <hello@flagseeker.com>
2024-10-06 01:42:24 +0000

    add main_test with goldens

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,13 +3,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_template/main.dart' as app;
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('main', () {
-    testWidgets('renders SplashScreen then MainApp',
-        (WidgetTester tester) async {
+    testWidgets('renders SplashScreen initially', (WidgetTester tester) async {
       app.main();
-      expect(find.byType(app.SplashScreen), findsOneWidget);
+      await expectLater(
+        find.byType(app.SplashScreen),
+        matchesGoldenFile('goldens/splash_screen.png'),
+      );
+    });
+    testWidgets('renders MainApp after pump', (WidgetTester tester) async {
+      app.main();
       await tester.pump();
-      expect(find.byType(app.MainApp), findsOneWidget);
+      await expectLater(
+        find.byType(app.MainApp),
+        matchesGoldenFile('goldens/main_app.png'),
+      );
     });
   });
 }
